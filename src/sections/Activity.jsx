4fa8ba682e7c876d7ecb25fd75357419790b0a2e
Andoyyy-rakon <polidarioandrewlloyd@gmail.com>
2025-09-09 20:37:35 +0800

    newUpdate

--- src/sections/Activity.jsx
+++ src/sections/Activity.jsx
@@ -30,38 +30,38 @@ const Activity = () => {
 
 
   return (
-    <div className=' min-h-screen w-full bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))]
+    <div className='dark:bg-[#1A1A26] dark:bg-none min-h-screen w-full bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))]
 from-[#52525b]
 via-[#a1a1aa]
 to-[#e4e4e7]
 '>
 
   <div className='h-screen  max-xl:w-[80%]   w-[50%] m-auto relative'>
-    <div className="w-[100%] border-b-2 border-slate-950 pt-12">
-      <div className="bg-black w-fit text-white p-2 text-[20px] font-medium te rounded-tr-2xl tracking-[0.12em]">Acitivity</div>
+    <div className="w-[100%] border-b-2 dark:border-[#2A2A3B]  border-slate-950 pt-12">
+      <div className="bg-black w-fit dark:bg-[#2A2A3B]  text-white p-2 text-[20px] font-medium te rounded-tr-2xl tracking-[0.12em]">Acitivity</div>
     </div>
 
-    <p className="font-normal text-lg leading-normal "> Below is a record of your past conversations. You can review your activity at any time or remove items you no longer need.</p>
+    <p className="dark:text-[#E0E0E0] font-normal text-lg leading-normal "> Below is a record of your past conversations. You can review your activity at any time or remove items you no longer need.</p>
 
         {history.map((item,index)=>(
-        <div key={item.content} className="pt-10 pb-2 px-3 border-b-2 border-black flex flex-col gap-2 ">
+        <div key={item.content} className="dark:border-[#2A2A3B] pt-10 pb-2 px-3 border-b-2 border-black flex flex-col gap-2 ">
         <div className="flex justify-between">
         <div className="flex items-center gap-2">
           <img src={assets.AiNgOdnumLogo} alt="" className="w-7"/>
-          <p className="font-normal text-slate-800">Ai ng odnum</p>
+          <p className="dark:text-[#E0E0E0] font-normal text-slate-800">Ai ng odnum</p>
         </div>
           
           <div className="flex gap-7 items-center">
-            <p className="tracking-normal text-sm font-bold text-slate-800">{item.date}</p>
-            <FontAwesomeIcon icon="fa-solid fa-x" onClick={()=>deleteActivity(index)} className="cursor-pointer hover:scale-125 transition-all duration-300" />
+            <p className="dark:text-[#E0E0E0] tracking-normal text-sm font-bold text-slate-800">{item.date}</p>
+            <FontAwesomeIcon icon="fa-solid fa-x" onClick={()=>deleteActivity(index)} className="dark:text-[#E0E0E0] cursor-pointer hover:scale-125 transition-all duration-300" />
 
           </div>
           
         </div>
 
         <div className="pl-9">
-          <h1 className="text-[27px] max-md:text-[25px] font-semibold text-black mb-4">{item.content}</h1>
-          <p className="tracking-widest text-sm font-bold text-slate-800">{item.time}</p>
+          <h1 className="dark:text-[#E0E0E0] text-[27px] max-md:text-[25px] font-semibold text-black mb-4">{item.content}</h1>
+          <p className="dark:text-[#E0E0E0] tracking-widest text-sm font-bold text-slate-800">{item.time}</p>
         </div>
 
 
@@ -71,7 +71,7 @@ to-[#e4e4e7]
         
 
         <div className="text-3xl absolute right-0 mt-10 mr-5 cursor-pointer " onClick={()=>navigate("/")}>
-          <FontAwesomeIcon icon="fa-solid fa-arrow-left" className="hover:scale-125 duration-300 transition-all" />
+          <FontAwesomeIcon icon="fa-solid fa-arrow-left" className="dark:text-[#E0E0E0] hover:scale-125 duration-300 transition-all" />
 
         </div>
 
